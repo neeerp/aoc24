5908e6af1a373caeea2bfcdb65967ff813dff68c
David Goldstein <goldstein.g.david@gmail.com>
2024-12-01 06:39:14 -0500

    read input from disk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let data = fs::read_to_string("inputs/day1p1_sample.txt").expect("Unable to read input!");
+    println!("{}", data);
 }
