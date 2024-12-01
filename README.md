ecbbe2b66b9628f5954d5c2da1ee4ecff720f9da
David Goldstein <goldstein.g.david@gmail.com>
2024-12-01 08:00:58 -0500

    Use clap to parse day arguments

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 This time, we're solving it in Rust...
 
 I don't really know what I'm doing but hopefully by Christmas I'll have half of a clue!
+
+## Instructions
+This project builds into a little CLI tool with a simple `-d <DAY>` argument; if no day is provided, then all days are run.
+
+To pass arguments through `cargo`, you'll need to signal to `cargo` that you're done giving it arguments using two hyphens:
+
+```sh
+cargo run -- -d 1
+```
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
+use clap::Parser;
+
 use day1::run_day1;
 
 mod day1;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The day to run; if 0 is given, all days are run.
+    #[arg(short, long, default_value_t = 0)]
+    day: u8,
+}
+
 fn main() {
+    let args = Args::parse();
+
+
+    match args.day {
+        0 => run_all(),
+        1 => run_day1(),
+        _ => todo!("Day {} is not upon us yet!", args.day)
+    }
+
+
+}
+
+fn run_all() {
     run_day1();
 }
