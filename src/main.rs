bac1ea93eb7ab6af9714e6824c12da848a124e9e
David Goldstein <goldstein.g.david@gmail.com>
2024-12-01 07:06:52 -0500

    Add tests to run sample input

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use std::{fs, iter::zip};
 
 fn main() {
-    let data = fs::read_to_string("inputs/day1p1_sample.txt").expect("Unable to read input!");
-    let result = day1p1(data.trim());
-    println!("{}", result);
+    todo!("Implement a main function, I guess?");
 }
 
-fn day1p1(input: &str) -> isize {
+fn day1p1(input: String) -> isize {
     let (mut first, mut second): (Vec<isize>, Vec<isize>) = input.split('\n').fold((vec![], vec![]), |(mut first, mut second), line| {
         let parts: Vec<&str> = line.split("   ").collect();
         match parts[..] {
@@ -24,4 +22,19 @@ fn day1p1(input: &str) -> isize {
 
     let sum: isize = zip(first, second).map(|(a, b)| (a - b).abs()).sum();
     sum
+}
+
+fn read_input(file_name: &str) -> String {
+    fs::read_to_string(file_name).expect("Unable to read input!").trim().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works_on_d1p1_sample() {
+        let input = read_input("inputs/day1p1_sample.txt");
+        assert_eq!(day1p1(input), 11);
+    }
 }
