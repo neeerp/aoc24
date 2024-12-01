33108128f2886246d9a11f5c5ae78101da85c80e
David Goldstein <goldstein.g.david@gmail.com>
2024-12-01 07:00:28 -0500

    Janky first implementation of d1p1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
-use std::fs;
+use std::{fs, iter::zip};
 
 fn main() {
     let data = fs::read_to_string("inputs/day1p1_sample.txt").expect("Unable to read input!");
-    println!("{}", data);
+    let result = day1p1(data.trim());
+    println!("{}", result);
+}
+
+fn day1p1(input: &str) -> isize {
+    let (mut first, mut second): (Vec<isize>, Vec<isize>) = input.split('\n').fold((vec![], vec![]), |(mut first, mut second), line| {
+        let parts: Vec<&str> = line.split("   ").collect();
+        match parts[..] {
+            [a, b] => {
+                first.push(a.parse().unwrap());
+                second.push(b.parse().unwrap());
+                (first, second)
+            }
+            _ => panic!("Oops!")
+        }
+    });
+
+    first.sort();
+    second.sort();
+
+    let sum: isize = zip(first, second).map(|(a, b)| (a - b).abs()).sum();
+    sum
 }
