ecb29313d071535cc6153af98c43c342799ae64b
David Goldstein <goldstein.g.david@gmail.com>
2024-12-02 12:00:49 -0500

    Solve day 2 part 2 with a brute-forcey solution

--- src/day2.rs
+++ src/day2.rs
@@ -3,26 +3,46 @@ use aoc_runner_derive::aoc;
 #[aoc(day2, part1)]
 pub fn part1(input: &str) -> u32 {
     let records = parse(input);
-    records.iter().map(|nums| {
-        if is_valid(nums) {
-            1
-        } else {
+    records
+        .iter()
+        .map(|nums| if is_valid(nums) { 1 } else { 0 })
+        .sum()
+}
+
+#[aoc(day2, part2)]
+pub fn part2(input: &str) -> u32 {
+    let records = parse(input);
+    records
+        .iter()
+        .map(|nums| {
+            if is_valid(nums) {
+                return 1;
+            }
+
+            for to_drop in 0..nums.len() {
+                let mut nums_copy = nums.clone();
+                nums_copy.remove(to_drop);
+
+                if is_valid(&nums_copy) {
+                    return 1;
+                }
+            }
             0
-        }
-    }).sum()
+        })
+        .sum()
 }
 
 pub fn parse(input: &str) -> Vec<Vec<i8>> {
-    input.lines().map(|line| {
-        line.split(' ').map(|c| c.parse().unwrap()).collect()
-    }).collect()
+    input
+        .lines()
+        .map(|line| line.split(' ').map(|c| c.parse().unwrap()).collect())
+        .collect()
 }
 
 pub fn is_valid(nums: &[i8]) -> bool {
     let asc = nums[0] < nums[1];
 
-    nums
-        .iter()
+    nums.iter()
         .zip(nums.iter().skip(1))
         .fold(true, |valid, (left, right)| {
             let mono_step = left < right && asc || (left > right && !asc);
@@ -50,5 +70,11 @@ mod tests {
     fn part1_sample() {
         let input = read_input("input/2024/sample/day2.txt");
         assert_eq!(part1(&input), 2);
+    }
+
+    #[test]
+    fn part2_sample() {
+        let input = read_input("input/2024/sample/day2.txt");
+        assert_eq!(part2(&input), 4);
     }
 }
